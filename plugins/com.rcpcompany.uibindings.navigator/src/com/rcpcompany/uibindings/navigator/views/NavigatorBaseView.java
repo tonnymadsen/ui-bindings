f182b7c235638fccd58bf1c431132bc0e01dbbb5
Tonny Madsen, The RCP Company <devnull@localhost>
2010-06-24 12:53:27 +0200

    NavigatorView now implements  ISetSelection

--- plugins/com.rcpcompany.uibindings.navigator/src/com/rcpcompany/uibindings/navigator/views/NavigatorBaseView.java
+++ plugins/com.rcpcompany.uibindings.navigator/src/com/rcpcompany/uibindings/navigator/views/NavigatorBaseView.java
@@ -6,11 +6,13 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.ui.part.ISetSelectionTarget;
 import org.eclipse.ui.part.ViewPart;
 
 import com.rcpcompany.uibindings.IBindingContext;
@@ -23,9 +25,11 @@ import com.rcpcompany.utils.logging.LogUtils;
 /**
  * The common base class for the navigation viewer.
  * 
+ * TODO: add follow selection
+ * 
  * @author Tonny Madsen, The RCP Company
  */
-public class NavigatorBaseView extends ViewPart implements IExecutableExtension {
+public class NavigatorBaseView extends ViewPart implements IExecutableExtension, ISetSelectionTarget {
 	/**
 	 * The advisor used for this view
 	 */
@@ -71,6 +75,11 @@ public class NavigatorBaseView extends ViewPart implements IExecutableExtension 
 	 */
 	public void collapseAll() {
 		myTreeViewer.collapseAll();
+	}
+
+	@Override
+	public void selectReveal(ISelection selection) {
+		myTreeViewer.setSelection(selection, true);
 	}
 
 	@Override
