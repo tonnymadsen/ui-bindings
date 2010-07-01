194e1802b18b52e289cf48e815d8456ccdd8e8e7
Tonny Madsen, The RCP Company <devnull@localhost>
2010-07-01 09:59:21 +0200

    Cleaned up

--- plugins/com.rcpcompany.uibindings.shop/src/com/rcpcompany/uibindings/shop/views/ShopNumbersView.java
+++ plugins/com.rcpcompany.uibindings.shop/src/com/rcpcompany/uibindings/shop/views/ShopNumbersView.java
@@ -1,14 +1,18 @@
 package com.rcpcompany.uibindings.shop.views;
 
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import com.rcpcompany.uibindings.tests.shop.Shop;
 import com.rcpcompany.uibindings.tests.shop.ShopFactory;
 import com.rcpcompany.uibindings.utils.IBindingContextSelectionProvider;
 import com.rcpcompany.uibindings.utils.IFormCreator;
 
+/**
+ * Simple view to show some very basic information about the {@link Shop}.
+ * 
+ * @author Tonny Madsen, The RCP Company
+ */
 public class ShopNumbersView extends ViewPart {
 
 	private IFormCreator myForm;
@@ -20,17 +24,10 @@ public class ShopNumbersView extends ViewPart {
 		myForm.addField("name");
 		myForm.addField("nextOrderNo");
 		myForm.addField("nextCustomerNo");
-		myForm.addField("tmpdir");
+		myForm.addField("tmpDir");
 
 		myForm.finish();
 		IBindingContextSelectionProvider.Factory.adapt(myForm.getContext(), getSite());
-
-		ShopFactory.eINSTANCE.getShop().eAdapters().add(new AdapterImpl() {
-			@Override
-			public void notifyChanged(Notification msg) {
-				System.out.println("shop change " + msg);
-			}
-		});
 	}
 
 	@Override
