0da5db093e6ff9f92e80e3e69ae442c977b00523
Tonny Madsen, The RCP Company <devnull@localhost>
2010-08-31 08:49:28 +0200

    Added some new SWT dump methods

---
+++ plugins/com.rcpcompany.utils.logging/src/com/rcpcompany/utils/logging/SWTEventUtils.java
@@ -0,0 +1,57 @@
+package com.rcpcompany.utils.logging;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+import com.rcpcompany.utils.basic.ToStringUtils;
+
+/**
+ * This class includes a number of utility classes that can make it easier to develop and test SWT
+ * based applications.
+ * 
+ * @author Tonny Madsen, The RCP Company
+ */
+public final class SWTEventUtils {
+	private SWTEventUtils() {
+
+	}
+
+	public final static Listener SWT_EVENT_LISTENER = new Listener() {
+		@Override
+		public void handleEvent(Event event) {
+			LogUtils.debug(this, ToStringUtils.toString(event));
+		}
+	};
+
+	/**
+	 * Logs all SWT events on the specified control.
+	 * 
+	 * @param c the control in question
+	 */
+	public static void swtListen(Control c) {
+		for (int i = SWT.None; i < SWT.ImeComposition; i++) {
+			c.addListener(i, SWT_EVENT_LISTENER);
+		}
+	}
+
+	/**
+	 * Runs the specified {@link Runnable} while debugging the SWT events.
+	 * 
+	 * @param runnable the {@link Runnable} to run
+	 */
+	public static void swtDisplayListen(Runnable runnable) {
+		try {
+			for (int i = SWT.None; i < SWT.ImeComposition; i++) {
+				Display.getCurrent().addFilter(i, SWT_EVENT_LISTENER);
+			}
+			runnable.run();
+		} finally {
+			for (int i = SWT.None; i < SWT.ImeComposition; i++) {
+				Display.getCurrent().removeFilter(i, SWT_EVENT_LISTENER);
+			}
+		}
+	}
+}
