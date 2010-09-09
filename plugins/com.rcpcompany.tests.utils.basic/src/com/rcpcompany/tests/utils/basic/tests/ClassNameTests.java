aa955bc16c67c5cfc5ce7dfc18191aaacd888281
Tonny Madsen, The RCP Company <devnull@localhost>
2010-09-09 13:52:43 +0200

    Added comment

--- plugins/com.rcpcompany.tests.utils.basic/src/com/rcpcompany/tests/utils/basic/tests/ClassNameTests.java
+++ plugins/com.rcpcompany.tests.utils.basic/src/com/rcpcompany/tests/utils/basic/tests/ClassNameTests.java
@@ -1,13 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009 The RCP Company and Others
  * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     The RCP Company - initial API and implementation
+ * 
+ * Contributors: The RCP Company - initial API and implementation
  *******************************************************************************/
 package com.rcpcompany.tests.utils.basic.tests;
 
@@ -17,8 +15,13 @@ import org.junit.Test;
 
 import com.rcpcompany.utils.basic.ClassUtils;
 
+/**
+ * Tests of {@link ClassUtils}.
+ * 
+ * @author Tonny Madsen, The RCP Company
+ */
 public class ClassNameTests {
-	private Object anonymous = new Object() {
+	private final Object anonymous = new Object() {
 	};
 
 	private class Dummy {
@@ -29,14 +32,14 @@ public class ClassNameTests {
 	public void testGetLastClassNameString() {
 		assertEquals("String", ClassUtils.getLastClassName("java.lang.String"));
 		assertEquals("Dummy", ClassUtils.getLastClassName(Dummy.class.getName()));
-		assertEquals(ClassUtils.getLastClassName(this.getClass().getName()) + "$1", ClassUtils
-				.getLastClassName(anonymous.getClass().getName()));
+		assertEquals(ClassUtils.getLastClassName(this.getClass().getName()) + "$1",
+				ClassUtils.getLastClassName(anonymous.getClass().getName()));
 	}
 
 	@Test
 	public void testGetLastClassNameObject() {
 		assertEquals("Dummy", ClassUtils.getLastClassName(new Dummy()));
-		assertEquals(ClassUtils.getLastClassName(this.getClass().getName()) + "$1", ClassUtils
-				.getLastClassName(anonymous));
+		assertEquals(ClassUtils.getLastClassName(this.getClass().getName()) + "$1",
+				ClassUtils.getLastClassName(anonymous));
 	}
 }
