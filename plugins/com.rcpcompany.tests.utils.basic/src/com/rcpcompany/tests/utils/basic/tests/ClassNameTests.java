9e1087fa0ae0eb66825b246a9fae13af913205e8
Tonny Madsen, The RCP Company <devnull@localhost>
2010-09-17 21:44:59 +0200

    Format and copyright updated

--- plugins/com.rcpcompany.tests.utils.basic/src/com/rcpcompany/tests/utils/basic/tests/ClassNameTests.java
+++ plugins/com.rcpcompany.tests.utils.basic/src/com/rcpcompany/tests/utils/basic/tests/ClassNameTests.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 The RCP Company and Others
- * 
- * All rights reserved. This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * Copyright (c) 2007, 2010 The RCP Company and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: The RCP Company - initial API and implementation
+ *
+ * Contributors:
+ *     The RCP Company - initial API and implementation
  *******************************************************************************/
 package com.rcpcompany.tests.utils.basic.tests;
 
--- plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/uiAttributeFactories/contentAdapters/CComboContentAdapter.java
+++ plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/uiAttributeFactories/contentAdapters/CComboContentAdapter.java
@@ -1,10 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2007, 2010 The RCP Company and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation
+ *
+ * Contributors:
+ *     The RCP Company - initial API and implementation
  *******************************************************************************/
 package com.rcpcompany.uibindings.internal.uiAttributeFactories.contentAdapters;
 
