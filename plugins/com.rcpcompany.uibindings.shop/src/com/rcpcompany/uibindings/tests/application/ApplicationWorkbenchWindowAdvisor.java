e8d4567a69342942708e767a72866d8ee6b6302b
Tonny Madsen, The RCP Company <devnull@localhost>
2010-12-03 23:07:49 +0100

    Incomplete - task : Offer to save shop on exit

--- plugins/com.rcpcompany.uibindings.shop/src/com/rcpcompany/uibindings/tests/application/ApplicationWorkbenchWindowAdvisor.java
+++ plugins/com.rcpcompany.uibindings.shop/src/com/rcpcompany/uibindings/tests/application/ApplicationWorkbenchWindowAdvisor.java
@@ -10,13 +10,23 @@
  *******************************************************************************/
 package com.rcpcompany.uibindings.tests.application;
 
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchCommandConstants;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 
+import com.rcpcompany.uibindings.IManager;
 import com.rcpcompany.uibindings.utils.IGlobalNavigationManager;
+import com.rcpcompany.utils.logging.LogUtils;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -43,5 +53,32 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		super.postWindowOpen();
 
 		IGlobalNavigationManager.Factory.createManager(getWindowConfigurer().getWindow());
+	}
+
+	@Override
+	public boolean preWindowShellClose() {
+		final IManager manager = IManager.Factory.getManager();
+		final EditingDomain editingDomain = manager.getEditingDomain();
+		int res = 1; // == NO
+		if (editingDomain.getCommandStack().canUndo()) {
+			final IWorkbenchWindow window = getWindowConfigurer().getWindow();
+			final MessageDialog dialog = new MessageDialog(window.getShell(), "Save Shop?", null,
+					"Changes has been made to the shop. Save these?", MessageDialog.QUESTION, new String[] {
+							IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL, IDialogConstants.CANCEL_LABEL }, 0);
+			res = dialog.open();
+			if (res == 0) {
+				final ICommandService cs = (ICommandService) window.getService(ICommandService.class);
+				final IHandlerService hs = (IHandlerService) window.getService(IHandlerService.class);
+
+				try {
+					final String c = manager.getCommandIDs().get(IWorkbenchCommandConstants.FILE_SAVE);
+					final ParameterizedCommand command = cs.deserialize(c);
+					hs.executeCommand(command, null);
+				} catch (final Exception ex) {
+					LogUtils.error(this, ex);
+				}
+			}
+		}
+		return res != 2;
 	}
 }
