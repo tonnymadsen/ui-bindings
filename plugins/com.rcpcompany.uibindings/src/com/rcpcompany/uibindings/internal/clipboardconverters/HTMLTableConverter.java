304cc27b14487ad7bf8b9b757e91bb6fc0f252b7
Tonny Madsen, The RCP Company <devnull@localhost>
2010-12-13 08:20:15 +0100

    Disabled

--- plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/clipboardconverters/HTMLTableConverter.java
+++ plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/clipboardconverters/HTMLTableConverter.java
@@ -1,17 +1,9 @@
 package com.rcpcompany.uibindings.internal.clipboardconverters;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.eclipse.swt.dnd.Clipboard;
-import org.eclipse.swt.dnd.HTMLTransfer;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -34,55 +26,58 @@ public class HTMLTableConverter implements IClipboardConverter {
 
 	@Override
 	public String[][] convert(Clipboard clipboard) {
-		String content = (String) clipboard.getContents(HTMLTransfer.getInstance());
-		if (content == null) return null;
+		return null;
+		// String content = (String) clipboard.getContents(HTMLTransfer.getInstance());
+		// if (content == null) return null;
+		//
+		// /*
+		// * Massage the content a little to get rid of a few bad eggs...
+		// *
+		// * - remove all attributes!!!
+		// *
+		// * - remove all visual tags except p, as these often does not match
+		// *
+		// * - replace <p> with newline
+		// */
+		// content = content.replaceAll("<(/?[a-z:.]+) [^>]*>", "<$1>");
+		// if (!content.contains("<td>")) return null;
+		// content = content.replaceAll(">[\t \r\n]+<", "><");
+		// content = content.replaceAll("</?(br|span|a|div|font|col) *>", "");
+		// content = content.replaceAll("<p *>", "\n");
+		// content = content.replaceAll("</p *>", "");
+		//
+		// final InputStream is = new StringBufferInputStream(content);
+		//
+		// try {
+		// final SAXParserFactory factory = SAXParserFactory.newInstance();
+		// final SAXParser p = factory.newSAXParser();
+		//
+		// //
+		// p.getXMLReader().setFeature("http://apache.org/xml/features/continue-after-fatal-error",
+		// // true);
+		//
+		// final DefaultHandler handler = new TableHandleBase();
+		// myElements.clear();
 		// LogUtils.debug(this, "Test:\n" + content + "\n<<<<");
-
-		/*
-		 * Massage the content a little to get rid of a few bad eggs...
-		 * 
-		 * - remove all attributes!!!
-		 * 
-		 * - remove all visual tags except p, as these often does not match
-		 * 
-		 * - replace <p> with newline
-		 */
-		content = content.replaceAll("<(/?[a-z:.]+) [^>]*>", "<$1>");
-		content = content.replaceAll(">[\t \r\n]+<", "><");
-		content = content.replaceAll("</?(br|span|a|div|font) *>", "");
-		content = content.replaceAll("<p *>", "\n");
-		content = content.replaceAll("</p *>", "");
-
-		final InputStream is = new StringBufferInputStream(content);
-
-		try {
-			final SAXParserFactory factory = SAXParserFactory.newInstance();
-			final SAXParser p = factory.newSAXParser();
-
-			// p.getXMLReader().setFeature("http://apache.org/xml/features/continue-after-fatal-error",
-			// true);
-
-			final DefaultHandler handler = new TableHandleBase();
-			myElements.clear();
-			p.parse(is, handler);
-		} catch (final SAXException ex) {
-			// TODO Auto-generated catch block
-			LogUtils.error(this, ex);
-		} catch (final IOException ex) {
-			// Should never happen
-			LogUtils.error(this, ex);
-			return null;
-		} catch (final ParserConfigurationException ex) {
-			// TODO Auto-generated catch block
-			LogUtils.error(this, ex);
-			return null;
-		}
-
-		final String[][] result = new String[myElements.size()][];
-		for (int i = 0; i < result.length; i++) {
-			result[i] = myElements.get(i).toArray(new String[0]);
-		}
-		return result;
+		// p.parse(is, handler);
+		// } catch (final SAXException ex) {
+		// // TODO Auto-generated catch block
+		// LogUtils.error(this, ex);
+		// } catch (final IOException ex) {
+		// // Should never happen
+		// LogUtils.error(this, ex);
+		// return null;
+		// } catch (final ParserConfigurationException ex) {
+		// // TODO Auto-generated catch block
+		// LogUtils.error(this, ex);
+		// return null;
+		// }
+		//
+		// final String[][] result = new String[myElements.size()][];
+		// for (int i = 0; i < result.length; i++) {
+		// result[i] = myElements.get(i).toArray(new String[0]);
+		// }
+		// return result;
 	}
 
 	public final List<List<String>> myElements = new ArrayList<List<String>>();
@@ -96,7 +91,7 @@ public class HTMLTableConverter implements IClipboardConverter {
 	public class TableHandleBase extends DefaultHandler {
 		@Override
 		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-			// LogUtils.debug(this, "uri=" + uri + ", localName=" + localName + ", qName=" + qName);
+			LogUtils.debug(this, "uri=" + uri + ", localName=" + localName + ", qName=" + qName);
 			if ("tr".equals(qName)) {
 				myElements.add(new ArrayList<String>());
 			} else if ("td".equals(qName)) {
@@ -108,7 +103,7 @@ public class HTMLTableConverter implements IClipboardConverter {
 
 		@Override
 		public void endElement(String uri, String localName, String qName) throws SAXException {
-			// LogUtils.debug(this, "uri=" + uri + ", localName=" + localName + ", qName=" + qName);
+			LogUtils.debug(this, "uri=" + uri + ", localName=" + localName + ", qName=" + qName);
 			if ("td".equals(qName)) {
 				if (myNextElement.startsWith("\n")) {
 					myNextElement = myNextElement.substring(1);
