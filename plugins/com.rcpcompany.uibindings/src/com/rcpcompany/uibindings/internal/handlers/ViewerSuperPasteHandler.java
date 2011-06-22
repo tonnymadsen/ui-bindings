5df83edd0acbe4e8296616053669bba18888c70f
tonny madsen <tonny.madsen@rcp-company.com>
2011-06-22 22:22:22 +0200

    REOPENED - issue SIMA-1113: Super paste & super create http://jira.marintek.sintef.no/jira/browse/SIMA-1113

--- plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/handlers/ViewerSuperPasteHandler.java
+++ plugins/com.rcpcompany.uibindings/src/com/rcpcompany/uibindings/internal/handlers/ViewerSuperPasteHandler.java
@@ -139,7 +139,7 @@ public class ViewerSuperPasteHandler extends AbstractHandler implements IHandler
 					 * Make a new paste binding and add this to the assignmentMap...
 					 */
 					final WritableValue ov = new WritableValue("", String.class);
-					final IUIAttribute attribute = new SimpleUIAttribute(null, null, ov, true);
+					final IUIAttribute attribute = new SimpleUIAttribute(null, "", ov, true);
 					// TODO: Why are we sure about this being a value?
 					final IValueBinding pasteBinding = context.addBinding().model(b.getModelObservableValue())
 							.ui(attribute);
