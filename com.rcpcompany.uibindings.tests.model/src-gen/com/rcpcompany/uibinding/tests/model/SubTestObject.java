73b549ee3926b92219b2aa1033f8f7ff79bf13c5
Tonny Madsen <tonny.madsen@gmail.com>
2012-09-14 21:36:14 +0200

    Revert "Revert "Revert "Revert "#31: Convert ex+shop.ecore+genmodel to xcore http://github.com/tonnymadsen/ui-bindings/issues/issue/31""""
    This reverts commit 00326c589051c537dbb886a4c128af6dff752abb.

---
+++ com.rcpcompany.uibindings.tests.model/src-gen/com/rcpcompany/uibinding/tests/model/SubTestObject.java
@@ -0,0 +1,15 @@
+/**
+ */
+package com.rcpcompany.uibinding.tests.model;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Sub Test Object</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see com.rcpcompany.uibinding.tests.model.TestModelPackage#getSubTestObject()
+ * @generated
+ */
+public interface SubTestObject extends TestObject {
+} // SubTestObject
