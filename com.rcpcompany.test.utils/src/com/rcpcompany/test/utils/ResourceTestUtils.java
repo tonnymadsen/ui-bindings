dc5761d70e117f88d3d904b0cf636d40e8647953
Tonny Madsen <tonny.madsen@gmail.com>
2012-12-02 16:59:46 +0100

    New robust versions of delete...(..

--- com.rcpcompany.test.utils/src/com/rcpcompany/test/utils/ResourceTestUtils.java
+++ com.rcpcompany.test.utils/src/com/rcpcompany/test/utils/ResourceTestUtils.java
@@ -2,14 +2,21 @@ package com.rcpcompany.test.utils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FilenameFilter;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
+
+import com.rcpcompany.utils.logging.LogUtils;
 
 /**
  * Various test utilities that are good for testing the workspace...
@@ -44,7 +51,7 @@ public class ResourceTestUtils {
 			fail("" + ex);
 		}
 		try {
-			p.delete(true, new NullProgressMonitor());
+			p.delete(true, true, new NullProgressMonitor());
 		} catch (final CoreException ex) {
 			ex.printStackTrace();
 			fail("" + ex);
@@ -56,7 +63,7 @@ public class ResourceTestUtils {
 	 * Deletes everything in the workspace.
 	 */
 	public static void deleteEverything() {
-		for (final IProject p : ROOT.getProjects()) {
+		for (final IProject p : ROOT.getProjects(IWorkspaceRoot.INCLUDE_HIDDEN)) {
 			try {
 				p.close(new MonitoredMonitor());
 			} catch (final CoreException ex) {
@@ -64,12 +71,26 @@ public class ResourceTestUtils {
 				fail("" + ex);
 			}
 			try {
-				p.delete(true, new NullProgressMonitor());
+				p.delete(true, true, new NullProgressMonitor());
 			} catch (final CoreException ex) {
 				ex.printStackTrace();
 				fail("" + ex);
 			}
 		}
-		assertEquals("no projects left", 0, ROOT.getProjects().length);
+		assertEquals("no projects left", 0, ROOT.getProjects(IWorkspaceRoot.INCLUDE_HIDDEN).length);
+
+		final IPath rootLocation = ROOT.getLocation();
+		final File rootFile = rootLocation.toFile();
+		assertTrue("ROOT does not exist", rootFile.exists());
+		rootFile.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				LogUtils.debug(this, "file name=" + name);
+				if (name.equals(".metadata"))
+					return false;
+				fail("File exists after delete: " + name);
+				return false;
+			}
+		});
 	}
 }
