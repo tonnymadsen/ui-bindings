6d6313a06da0544b67d57c7b888d6750c698b54f
Tonny Madsen <tonny.madsen@gmail.com>
2013-07-29 22:19:26 +0200

    Test of DS of logging

---
+++ com.rcpcompany.utils.logging.tests/src/com/rcpcompany/utils/logging/tests/DSTest.java
@@ -0,0 +1,22 @@
+package com.rcpcompany.utils.logging.tests;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import com.rcpcompany.test.utils.DSTestUtils;
+
+/**
+ * Tests that all the XML files in OSGI-INF is listed in the MANIFEST.MF.
+ * 
+ * @author Tonny Madsen, tma@bording.dk
+ */
+public class DSTest {
+	@Rule
+	public Timeout globalTimeout = new Timeout(60000); // 60 seconds max per method tested
+
+	@Test
+	public void testDSFilesPresent() {
+		DSTestUtils.checkDSFiles("com.rcpcompany.utils.logging");
+	}
+}
